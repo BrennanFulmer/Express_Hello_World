fadfdf92ddcb0c70d0b8e4ba0011de9c5aa31720
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-05-07 19:47:11 -0400

    modified for Heroku deployment

--- index.js
+++ index.js
@@ -3,10 +3,10 @@
   http://localhost:3000/
 */
 
-/*
 // Express hello world example
 const express = require("express");
 const app = express();
+const port = process.env.PORT || '3000';
 
 app.set("view engine", "hbs");
 
@@ -14,28 +14,33 @@ app.get("/", (req, res) => {
   res.render("greeting");
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log("Example app listening on port 3000!");
 });
-*/
+/**/
 
-
+/*
 // Viking Code School example
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.set('view engine', 'hbs')
+app.set("view engine", "hbs");
 
-app.get('/', (req, res) => {
-  res.render('hello', { name: "Viking" })
-})
+app.use((req, res, next) => {
+  console.log(req.url + " — " + new Date());
+  next();
+});
 
-app.get('/person/:personName', (req, res) => {
-  const personName = req.params.personName
-  res.render('hello', { name: personName })
-})
+app.get("/", function(req, res) {
+  res.render("hello");
+});
+
+app.get("/names/:name", (req, res) => {
+  const name = req.params.name;
+  res.render("hello", { name });
+});
 
 app.listen(3001, () => {
-  console.log('Odin is listening on port 3001!')
-})
-/**/
+  console.log("Odin is listening on port 3001!");
+});
+*/
