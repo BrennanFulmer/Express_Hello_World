32113225d37b06243b6bd6ba54e4e5157e6d3292
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-05-07 19:03:09 -0400

    determined link problem was happening because that's not supposed to be how it works - this more of simulates a different path in its present form

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 /*
   link to view locally
   http://localhost:3000/
-  or
-  // viking link doesn't actually work anymore ???
-  http://localhost/names/< insert name >:3000/
 */
 
+/*
 // Express hello world example
 const express = require("express");
 const app = express();
@@ -19,19 +17,25 @@ app.get("/", (req, res) => {
 app.listen(3000, () => {
   console.log("Example app listening on port 3000!");
 });
-
-/*
-// Viking Code School example
-const express = require("express");
-const app = express();
-
-app.get("/names/:name", function(req, res) {
-  const name = req.params.name;
-  res.send("Hello " + name);
-});
-
-app.listen(3000, function() {
-  // This function is run when the app starts up.
-  console.log("Kemst þó hægt fari.");
-});
 */
+
+
+// Viking Code School example
+const express = require('express')
+const app = express()
+
+app.set('view engine', 'hbs')
+
+app.get('/', (req, res) => {
+  res.render('hello', { name: "Viking" })
+})
+
+app.get('/person/:personName', (req, res) => {
+  const personName = req.params.personName
+  res.render('hello', { name: personName })
+})
+
+app.listen(3001, () => {
+  console.log('Odin is listening on port 3001!')
+})
+/**/
