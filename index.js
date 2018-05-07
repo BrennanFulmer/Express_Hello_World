0ecd022e81589954bb818b1f7b09aaa08586c17a
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-05-07 18:28:26 -0400

    just going to move on even though the random name links thing doesn't work

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
 /*
-  link to view locally http://localhost:3000/
-
-  // Viking Code School example
+  link to view locally
+  http://localhost:3000/
+  or
+  // doesn't actually work ???
+  http://localhost/names/< insert name >:3000/
 */
-
-const express = require('express')
-const app = express()
-
-app.get('/', function (request, response) {
-  response.send('Hello Viking!')
-})
-
-app.listen(3000, function () {
-  // This function is run when the app starts up.
-  console.log('Kemst þó hægt fari.')
-})
 
 /*
-// Express hello world example
+// Viking Code School example
+const express = require("express");
+const app = express();
 
-const express = require('express')
-const app = express()
+app.get("/names/:name", function(req, res) {
+  const name = req.params.name;
+  res.send("Hello " + name);
+});
 
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, function() {
+  // This function is run when the app starts up.
+  console.log("Kemst þó hægt fari.");
+});
 */
+
+// Express hello world example
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(3000, () => console.log("Example app listening on port 3000!"));
